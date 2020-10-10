8c0b6f3c374378c0f1d55b90384ee94a67e68c7b
srivatsan2002 <72650970+srivatsan2002@users.noreply.github.com>
2020-10-10 15:36:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+function speak() {
+    var utterance=new SpeechSynthesisUtterance(speakdata);           
+    synthesis.speak(utterance);
+    }
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'jpg',
+    jpg_quality:100
+    });
+    
+    camera=document.getElementById("camera");
+    Webcam.attach("camera");
+    
+    function takepic() {
+    Webcam.snap(function(data_url){
+    document.getElementById("result").innerHTML='<img id="captured_pic" src="'+data_url+'">';
+    });
+    }
+    
+    console.log("ml5 version",ml5.version);
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/eIfhGsEPV/model.json",model_loaded);
+    
+    function model_loaded() {
+    console.log("model loaded!");
+    }
+    
+    function check() {
+    img=document.getElementById("captured_pic");
+    classifier.classify(img,check_result);
+    }
+    
+    function check_result(error,result) {
+    if (error) {
+    console.log(error);
+    }
+    else {
+    console.log(result);
+    document.getElementById("result_emotion_name").innerHTML=result[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=result[1].label;
+    if (result[0].label=="amazing") {
+        document.getElementById("update_emoji").innerHTML="&#x1F44C;";
+        speak("amazing");
+        }
+    if (result[0].label=="welcome") {
+    document.getElementById("update_emoji").innerHTML="&#128591;";
+    }
+    if (result[0].label=="rock") {
+    document.getElementById("update_emoji").innerHTML="&#129304;";
+    }
+    if (result[0].label=="victory") {
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+    }
+    if (result[0].label=="like") {
+        document.getElementById("update_emoji").innerHTML="&#128077;";
+        }
+        if (result[0].label=="punch") {
+            document.getElementById("update_emoji").innerHTML="&#128074;";
+            }
+            if (result[0].label=="dislike") {
+                document.getElementById("update_emoji").innerHTML="&#128078;";
+                }
+
+               if (result[1].label=="amazing") {
+                    document.getElementById("update_emoji2").innerHTML="&#x1F44C;";
+                    }         
+                if (result[1].label=="welcome") {
+    document.getElementById("update_emoji2").innerHTML="&#128591;";
+    }
+    if (result[1].label=="rock") {
+    document.getElementById("update_emoji2").innerHTML="&#129304;";
+    }
+    if (result[1].label=="victory") {
+    document.getElementById("update_emoji2").innerHTML="&#9996;";
+    }
+    if (result[1].label=="like") {
+        document.getElementById("update_emoji2").innerHTML="&#128077;";
+        }
+        if (result[1].label=="punch") {
+            document.getElementById("update_emoji2").innerHTML="&#128074;";
+            }
+            if (result[1].label=="dislike") {
+                document.getElementById("update_emoji2").innerHTML="&#128078;";
+                }
+    }
+    }
